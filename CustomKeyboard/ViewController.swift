393b86df14d16f5a48839bb19a776aa2970c3fec
Kosuke Nagano <gm.charlie@gmail.com>
2014-09-24 19:15:35 +0900

    Initial Commit

---
+++ CustomKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomKeyboard
+//
+//  Created by Kosuke Nagano on 2014/09/24.
+//  Copyright (c) 2014年 Kousuke Nagano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
